1a8c33357e900bb1b1bcd31074a668b4f0d975cd
Vruxachavda <78405611+vruxachavda@users.noreply.github.com>
2021-06-03 22:42:59 +0530

    Add files via upload

---
+++ day4 integration theme/themepart/logo.php
@@ -0,0 +1,7 @@
+<div id="header-wrapper">
+	<div id="header">
+		<div id="logo">
+			<h1><a href="#">Alex</a></h1>
+		</div>
+	</div>
+</div>
---
+++ day4 integration theme/themepart/sidebar.php
@@ -0,0 +1,4 @@
+<div id="sidebar">
+			<h2>News</h2>
+			<marquee>Welcome</marquee>
+</div>
