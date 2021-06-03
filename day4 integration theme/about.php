ebb3108a17ad12edc6cc46488eca0f4f356c8ad4
Vruxachavda <78405611+vruxachavda@users.noreply.github.com>
2021-06-03 22:30:06 +0530

    themework

---
+++ day4 integration theme/about.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>akash technolab</title>
+<link href="http://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" type="text/css" />
+<link href="http://fonts.googleapis.com/css?family=Coda:400,800" rel="stylesheet" type="text/css" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+
+	<?php
+      include './themepart/menu.php';
+      ?>
+	<!-- end #menu --> 
+
+
+<?php
+      include './themepart/logo.php';
+      ?>
+
+<div id="wrapper"> 
+	<!-- end #header -->
+	<div id="page-bgtop"></div>
+	<div id="page">
+		<div><img src="technolab.png" width="920" height="300" alt="" /></div>
+		<div id="content">
+			<div class="post">
+				<h2 class="title"><a href="#">About.</a></h2>
+				
+				<div style="clear: both;">&nbsp;</div>
+				<div class="entry">
+					welcome.
+                     We have rich experience of 11+ years in offering Website Design, Web Development, CMS, eCommerce Solutions, Mobile Apps and Digital Marketing Services to business of all statures whether you are a small business or a corporate company. Our team of young and skilled developers have expertise to deliver the perfect web and mobile apps solutions.
+
+				</div>
+			</div>
+			
+			<div style="clear: both;">&nbsp;</div>
+		</div>
+		<!-- end #content -->
+		<?php
+      include './themepart/sidebar.php';
+      ?>
+		<!-- end #sidebar -->
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<div id="page-bgbtm"></div>
+	<!-- end #page --> 
+</div>
+<?php
+      include './themepart/footer.php';
+      ?>
+<!-- end #footer -->
+</body>
+</html>
