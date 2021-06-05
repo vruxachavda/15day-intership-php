eb923ecfbe200057ed8ab3a7a1bdd9d6c36153ad
Vruxachavda <78405611+vruxachavda@users.noreply.github.com>
2021-06-06 00:11:01 +0530

    Create associative array.php

---
+++ day5 databaseconection/associative array.php
@@ -0,0 +1,8 @@
+<?php    
+$salary["Sonoo"]="350000";    
+$salary["John"]="450000";    
+$salary["Kartik"]="200000";    
+echo "Sonoo salary: ".$salary["Sonoo"]."<br/>";  
+echo "John salary: ".$salary["John"]."<br/>";  
+echo "Kartik salary: ".$salary["Kartik"]."<br/>";  
+?>    
