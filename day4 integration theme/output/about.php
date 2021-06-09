6a562cde9db9270c62c2a27cfd9b92120c176b60
Vruxachavda <78405611+vruxachavda@users.noreply.github.com>
2021-06-09 20:15:08 +0530

    Create about.php

---
+++ day4 integration theme/output/about.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>akash technolab</title>
+<link href="http://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" type="text/css" />
+<link href="http://fonts.googleapis.com/css?family=Coda:400,800" rel="stylesheet" type="text/css" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+
+	<?php
+      include './themepart/menu.php';
+      ?>
+	<!-- end #menu --> 
+
+
+<?php
