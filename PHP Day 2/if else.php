92449e3c127fe94dd50792a5d2382edc311b2c0d
Vruxachavda <78405611+vruxachavda@users.noreply.github.com>
2021-06-12 18:47:37 +0530

    Add files via upload

---
+++ PHP Day 2/if else.php
@@ -0,0 +1,14 @@
+<?php
+    $a = 20;
+    $b = 10;
+
+    if($a > $b){
+        echo "Value of A is bigger than B";
+    }
+    else if($a < $b){
+        echo "Value of A is smaller than B";
+    }
+    else{
+        echo "Value of A and B are same.";
+    }
+?>
